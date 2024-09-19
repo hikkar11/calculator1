d3e50e73ace6cb23034001ba203598bf971af9ec
hikkar11 <shizotiktok@mail.ru>
2024-09-19 23:36:32 +0500

    Update калькулятор.java

--- src/калькулятор.java
+++ src/калькулятор.java
@@ -2,43 +2,51 @@ import java.util.Scanner;
 
 public class калькулятор {
     public static void main(String[] args) {
-        String[] actions = {"-", "+", "*", "/"};
-        String[] regexActions = {"-", "\\+", "\\*", "/"};
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Введите выражение");
-        String exp = scn.nextLine();
-        int actionIndex=-1;
-        for (int i = 0; i < actions.length; i++){
-            if(exp.contains(actions[i])){
-                actionIndex = i;
-                break;
+            String[] actions = {"-", "+", "*", "/"};
+            String[] regexActions = {"-", "\\+", "\\*", "/"};
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Введите выражение");
+            String exp = scn.nextLine();
+
+            int actionIndex=-1;
+            for (int i = 0; i < actions.length; i++){
+                if(exp.contains(actions[i])){
+                    actionIndex = i;
+                    break;
+                }
             }
-        }
-        if (actionIndex==-1){
-            System.out.println("Некорректное значение");
-            return;
-        }
-        String[] data = exp.split(regexActions[actionIndex]);
-        int a,b;
+            if (actionIndex==-1){
+                System.out.println("Некорректное значение");
+                return;
+            }
 
-        a = Integer.parseInt(data[0]);
-        b = Integer.parseInt(data[1]);
+            String[] data = exp.split(regexActions[actionIndex]);
+            int a,b = 0;
 
-        int result;
-        switch (actions[actionIndex]){
-            case "+":
-                result = a+b;
-                break;
-            case "-":
-                result = a-b;
-                break;
-            case "*":
-                result = a*b;
-                break;
-            default:
-                result = a/b;
-                break;
-        }
-        System.out.println(result);
+            try {
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+            } catch (NumberFormatException ex){
+                System.out.println("Вы допустили ошибку при вводе");
+                return;
+            }
+
+            int result;
+            switch (actions[actionIndex]){
+                case "+":
+                    result = a+b;
+                    break;
+                case "-":
+                    result = a-b;
+                    break;
+                case "*":
+                    result = a*b;
+                    break;
+                default:
+                    result = a/b;
+                    break;
+            }
+            System.out.println(result);
+
     }
 }
